6971d53b9f853f88a1e2b85a6665f1e8750e7f65
YoboZorle <www>
2020-11-12 16:59:33 +0100

    fix android map, place meta data above activity below application

--- lib/src/auth/complete_profile_form.dart
+++ lib/src/auth/complete_profile_form.dart
@@ -5,7 +5,7 @@ import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:pickrr_app/src/auth/otp_verification.dart';
+import 'package:pickrr_app/src/home.dart';
 import 'package:pickrr_app/src/values/values.dart';
 
 class CompleteProfileForm extends StatefulWidget {
@@ -26,7 +26,7 @@ class _CompleteProfileFormState extends State<CompleteProfileForm> {
 
   Future getImage() async {
     final pickedFile =
-    await ImagePicker().getImage(source: ImageSource.gallery);
+        await ImagePicker().getImage(source: ImageSource.gallery);
     setState(() {
       _profileImage = File(pickedFile.path);
     });
@@ -51,119 +51,128 @@ class _CompleteProfileFormState extends State<CompleteProfileForm> {
                           child: ListView(
                               physics: const BouncingScrollPhysics(),
                               children: <Widget>[
-                             Container(
-                                      alignment: Alignment.centerLeft,
-                                      margin: const EdgeInsets.only(left: 15.0),
-                                      height: 25,
-                                      width: 80,
-                                      child: Text(
-                                        'Skip',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          fontFamily: 'Ubuntu',
-                                          color: AppColors.primaryText,
-                                          fontWeight: FontWeight.w600,
-                                        ),
+                            Row(
+                              children: [
+                                GestureDetector(
+                                  child: Container(
+                                    alignment: Alignment.centerLeft,
+                                    padding: const EdgeInsets.only(left: 15.0),
+                                    height: 30,
+                                    width: 80,
+                                    child: Text(
+                                      'Skip',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontFamily: 'Ubuntu',
+                                        color: AppColors.primaryText,
+                                        fontWeight: FontWeight.w600,
                                       ),
                                     ),
-                                SizedBox(height: 30),
-                                Text(
-                                  'Set Up Your Profile',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 22,
-                                    fontFamily: 'Ubuntu',
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w800,
                                   ),
+                                  onTap: (){
+
+                                  },
                                 ),
-                                SizedBox(height: 45),
-                                GestureDetector(
-                                  onTap: getImage,
-                                  child: Container(
-                                      alignment: Alignment.center,
-                                      child: Column(
-                                        children: <Widget>[
-                                          _profileImage == null
-                                              ? ClipOval(
+                              ],
+                            ),
+                            SizedBox(height: 25),
+                            Text(
+                              'Set Up Your Profile',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontFamily: 'Ubuntu',
+                                color: Colors.black,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            SizedBox(height: 45),
+                            GestureDetector(
+                              onTap: getImage,
+                              child: Container(
+                                  alignment: Alignment.center,
+                                  child: Column(
+                                    children: <Widget>[
+                                      _profileImage == null
+                                          ? ClipOval(
                                               child: Image.asset(
-                                                'assets/images/placeholder.jpg',
-                                                fit: BoxFit.cover,
-                                                width: 95.0,
-                                                height: 95.0,
-                                              ))
-                                              : ClipOval(
+                                              'assets/images/placeholder.jpg',
+                                              fit: BoxFit.cover,
+                                              width: 95.0,
+                                              height: 95.0,
+                                            ))
+                                          : ClipOval(
                                               child: Image.file(
-                                                _profileImage,
-                                                fit: BoxFit.cover,
-                                                width: 95.0,
-                                                height: 95.0,
-                                              )),
-                                          SizedBox(height: 18),
-                                          Text('Upload photo',
-                                              style: TextStyle(
-                                                fontSize: 15,
-                                                fontFamily: 'Ubuntu',
-                                                color: Colors.grey,
-                                                fontWeight: FontWeight.w500,
-                                              )),
-                                        ],
-                                      )),
-                                ),
-                                SizedBox(height: 25),
-                                Container(
-                                    alignment: Alignment.center,
-                                    height: 55,
-                                    margin: EdgeInsets.only(
-                                        left: 20, right: 20, bottom: 10),
-                                    color: Colors.grey[200],
-                                    child: emailInput()),
-                                Container(
-                                  margin: EdgeInsets.only(left: 20),
-                                  child: Text('We\'ll send you your ride receipts.',
-                                      style: TextStyle(
-                                          fontFamily: 'Ubuntu',
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.grey,
-                                          fontSize: 14)),
-                                ),
-                                SizedBox(height: 30),
-                                Container(
-                                  height: 50,
-                                  margin: EdgeInsets.symmetric(horizontal: 20),
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                          child: Container(
-                                              height: 0.8,
-                                              width:
-                                              MediaQuery.of(context).size.width,
-                                              color: Colors.grey,
-                                              margin: EdgeInsets.only(right: 20))),
-                                      Text("OR",
-                                          style: TextStyle(color: Colors.grey)),
-                                      Expanded(
-                                          child: Container(
-                                              height: 0.8,
-                                              width:
-                                              MediaQuery.of(context).size.width,
-                                              color: Colors.grey,
-                                              margin: EdgeInsets.only(left: 20))),
+                                              _profileImage,
+                                              fit: BoxFit.cover,
+                                              width: 95.0,
+                                              height: 95.0,
+                                            )),
+                                      SizedBox(height: 18),
+                                      Text('Upload photo',
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                            fontFamily: 'Ubuntu',
+                                            color: Colors.grey,
+                                            fontWeight: FontWeight.w500,
+                                          )),
                                     ],
-                                  ),
-                                ),
-                                SizedBox(height: 30),
-                                Container(
-                                  height: 50,
-                                  margin: EdgeInsets.symmetric(horizontal: 20),
-                                  child: SignInButton(
-                                    Buttons.Google,
-                                    text: "Continue with Google",
-                                    elevation: 20,
-                                    onPressed: () {},
-                                  ),
-                                ),
-                              ])),
+                                  )),
+                            ),
+                            SizedBox(height: 25),
+                            Container(
+                                alignment: Alignment.center,
+                                height: 55,
+                                margin: EdgeInsets.only(
+                                    left: 20, right: 20, bottom: 10),
+                                color: Colors.grey[200],
+                                child: emailInput()),
+                            Container(
+                              margin: EdgeInsets.only(left: 20),
+                              child: Text('We\'ll send you your ride receipts.',
+                                  style: TextStyle(
+                                      fontFamily: 'Ubuntu',
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.grey,
+                                      fontSize: 14)),
+                            ),
+                            SizedBox(height: 30),
+                            Container(
+                              height: 50,
+                              margin: EdgeInsets.symmetric(horizontal: 20),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                      child: Container(
+                                          height: 0.8,
+                                          width:
+                                              MediaQuery.of(context).size.width,
+                                          color: Colors.grey,
+                                          margin: EdgeInsets.only(right: 20))),
+                                  Text("OR",
+                                      style: TextStyle(color: Colors.grey)),
+                                  Expanded(
+                                      child: Container(
+                                          height: 0.8,
+                                          width:
+                                              MediaQuery.of(context).size.width,
+                                          color: Colors.grey,
+                                          margin: EdgeInsets.only(left: 20))),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            Container(
+                              height: 50,
+                              margin: EdgeInsets.symmetric(horizontal: 20),
+                              child: SignInButton(
+                                Buttons.Google,
+                                text: "Continue with Google",
+                                elevation: 20,
+                                onPressed: () {},
+                              ),
+                            ),
+                          ])),
                     ])),
               ),
               Container(
@@ -183,7 +192,8 @@ class _CompleteProfileFormState extends State<CompleteProfileForm> {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) => OTPVerification(authId: null)));
+                                  builder: (context) =>
+                                      Home()));
                         },
                         color: AppColors.primaryText,
                         child: Text("Continue",
@@ -211,57 +221,57 @@ class _CompleteProfileFormState extends State<CompleteProfileForm> {
   }
 
   emailInput() => Container(
-    margin: EdgeInsets.only(left: 15),
-    child: Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.only(left: 0),
-            child: TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              cursorColor: AppColors.primaryText,
-              controller: _emailController,
-              style: TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'Ubuntu',
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400),
-              decoration: InputDecoration(
-                hintText: 'Input email',
-                hintStyle: TextStyle(
-                    fontSize: 20.0,
-                    fontFamily: 'Ubuntu',
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w400),
-                border: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                enabledBorder: InputBorder.none,
-                errorBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                suffixIcon: _emailController.text.isNotEmpty
-                    ? Padding(
-                  padding:
-                  const EdgeInsetsDirectional.only(start: 12.0),
-                  child: IconButton(
-                    iconSize: 16.0,
-                    icon: Icon(
-                      Icons.cancel,
-                      color: Colors.grey,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _emailController.clear();
-                      });
-                    },
+        margin: EdgeInsets.only(left: 15),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.only(left: 0),
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  cursorColor: AppColors.primaryText,
+                  controller: _emailController,
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Ubuntu',
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400),
+                  decoration: InputDecoration(
+                    hintText: 'Input email',
+                    hintStyle: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Ubuntu',
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w400),
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    suffixIcon: _emailController.text.isNotEmpty
+                        ? Padding(
+                            padding:
+                                const EdgeInsetsDirectional.only(start: 12.0),
+                            child: IconButton(
+                              iconSize: 16.0,
+                              icon: Icon(
+                                Icons.cancel,
+                                color: Colors.grey,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _emailController.clear();
+                                });
+                              },
+                            ),
+                          )
+                        : null,
                   ),
-                )
-                    : null,
+                ),
               ),
             ),
-          ),
+          ],
         ),
-      ],
-    ),
-  );
+      );
 }
--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,118 +1,60 @@
-/*
-*  main_widget.dart
-*  Pickrr
-*
-*  Created by Yobo Zorle.
-*  Copyright © 2020 Klynox. All rights reserved.
-    */
-
 import 'package:flutter/material.dart';
-import 'values/values.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+  GoogleMapController myMapController;
+  final Set<Marker> _markers = new Set();
+  static const LatLng _mainLocation = const LatLng(4.814340, 7.000848);
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: _mainLocation,
+              zoom: 15.6,
+            ),
+            markers: this.myMarker(),
+            mapType: MapType.normal,
+            zoomControlsEnabled: false,
+            onMapCreated: (controller) {
+              setState(() {
+                myMapController = controller;
+              });
+            },
+          ),
+        ),
+        Container(height: 200,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.white,
+        child: Text('Yobo Zorle'))
+      ],
+    )));
+  }
 
-    return Scaffold(
-      body: Container(
-        constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 255, 255, 255),
+  Set<Marker> myMarker() {
+    setState(() {
+      _markers.add(Marker(
+        // This marker id can be anything that uniquely identifies each marker.
+        markerId: MarkerId(_mainLocation.toString()),
+        position: _mainLocation,
+        infoWindow: InfoWindow(
+          title: 'Historical City',
+          snippet: '5 Star Rating',
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 199,
-              height: 63,
-              margin: EdgeInsets.only(left: 44, top: 64),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                      width: 89,
-                      height: 63,
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryBackground,
-                        border: Border.fromBorderSide(Borders.primaryBorder),
-                        borderRadius: Radii.k15pxRadius,
-                      ),
-                      child: Container(),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                      width: 89,
-                      height: 63,
-                      margin: EdgeInsets.only(left: 21),
-                      decoration: BoxDecoration(
-                        color: AppColors.secondaryBackground,
-                      ),
-                      child: Container(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: 187,
-              height: 66,
-              margin: EdgeInsets.only(left: 44, top: 16),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                      width: 89,
-                      height: 63,
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryBackground,
-                        boxShadow: [
-                          Shadows.primaryShadow,
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Yes",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: AppColors.primaryText,
-                              fontFamily: "Ubuntu",
-                              fontWeight: FontWeight.w400,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                      width: 60,
-                      height: 60,
-                      margin: EdgeInsets.only(left: 38, top: 6),
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryElement,
-                        border: Border.fromBorderSide(Borders.primaryBorder),
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                      ),
-                      child: Container(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+
+    return _markers;
   }
 }
