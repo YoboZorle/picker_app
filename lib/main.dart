2fd4d66122a44b233c6deb00123acd5cb00cace6
YoboZorle <yobozorle@gmail.com>
2020-12-14 15:47:32 +0100

    screen rotation fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
+import 'package:flutter/services.dart';
 import 'package:pickrr_app/src/app.dart';
 import 'package:pickrr_app/src/blocs/observer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = CustomBlocObserver();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(App());
 }
