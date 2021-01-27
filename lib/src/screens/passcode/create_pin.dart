e8b3602e8812f44c6ea42cbbd786bf421454bda7
MAC <mac@MacBook-Pro.local>
2021-01-27 12:55:34 +0100

    fix pin

--- lib/src/screens/passcode/create_pin.dart
+++ lib/src/screens/passcode/create_pin.dart
@@ -1,180 +1,259 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_awesome_alert_box/flutter_awesome_alert_box.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pickrr_app/src/blocs/authentication/bloc.dart';
-import 'package:pickrr_app/src/models/user.dart';
+import 'package:pinput/pin_put/pin_put.dart';
 
-class CreatePin extends StatelessWidget {
+class CreatePin extends StatefulWidget {
+  @override
+  CreatePinState createState() => CreatePinState();
+}
+
+class CreatePinState extends State<CreatePin> {
+  final _formKey = GlobalKey<FormState>();
+  final _pinPutController = TextEditingController();
+  final _pinPutFocusNode = FocusNode();
+  final _pageController = PageController();
+
+  int _pageIndex = 0;
+
+  final List<Widget> _pinPuts = [];
+
+  final List<Color> _bgColors = [
+    Colors.white,
+    const Color.fromRGBO(43, 36, 198, 1),
+    Colors.white,
+    const Color.fromRGBO(75, 83, 214, 1),
+    const Color.fromRGBO(43, 46, 66, 1),
+  ];
+
+  @override
+  void initState() {
+    _pinPuts.addAll([
+      onlySelectedBorderPinPut(),
+      darkRoundedPinPut(),
+      animatingBorders(),
+      boxedPinPutWithPreFilledSymbol(),
+      justRoundedCornersPinPut(),
+    ]);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
-        builder: (_, state) {
-      if (state is NonLoggedIn) {
-        WidgetsBinding.instance.addPostFrameCallback(
-            (_) => Navigator.pushReplacementNamed(context, '/'));
-      }
-      if (state.props.isEmpty) {
-        return Container();
-      }
-      User user = state.props[0];
-
-      return Scaffold(
-        body: SafeArea(
-          child: CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                backgroundColor: Colors.white,
-                brightness: Brightness.light,
-                centerTitle: true,
-                title: Text(
-                  'My Profile',
-                  style: TextStyle(
-                      fontFamily: "Ubuntu",
-                      fontSize: 17.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w800),
-                ),
-                leading: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(Icons.arrow_back_ios, color: Colors.black)),
-              ),
-              SliverList(
-                delegate: SliverChildListDelegate(
-                  [
-                    SizedBox(height: 10),
-                    Card(
-                      elevation: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 5.0, horizontal: 20),
-                        child: ListTile(
-                          title: Text(
-                            user.fullname,
-                            style: TextStyle(
-                                fontSize: 16.0,
-                                fontFamily: "Ubuntu",
-                                color: Colors.black87,
-                                fontWeight: FontWeight.w500,
-                                height: 1.6),
-                          ),
-                          subtitle: Text(
-                            'Full name',
-                            style: TextStyle(
-                                fontSize: 15.0,
-                                fontFamily: "Ubuntu",
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w400,
-                                height: 1.5),
-                          ),
-                          contentPadding: EdgeInsets.all(0),
-                          dense: true,
-                        ),
-                      ),
-                    ),
-                    Card(
-                      elevation: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 5.0, horizontal: 20),
-                        child: ListTile(
-                          title: Text(
-                            user.email,
-                            style: TextStyle(
-                                fontSize: 16.0,
-                                fontFamily: "Ubuntu",
-                                color: Colors.black87,
-                                fontWeight: FontWeight.w500,
-                                height: 1.6),
-                          ),
-                          subtitle: Text(
-                            'My email',
-                            style: TextStyle(
-                                fontSize: 15.0,
-                                fontFamily: "Ubuntu",
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w400,
-                                height: 1.5),
-                          ),
-                          contentPadding: EdgeInsets.all(0),
-                          dense: true,
-                        ),
-                      ),
-                    ),
-                    Card(
-                      elevation: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 5.0, horizontal: 20),
-                        child: ListTile(
-                          title: Text(
-                            '+${user.callingCode}${user.phone}',
-                            style: TextStyle(
-                                fontSize: 16.0,
-                                fontFamily: "Ubuntu",
-                                color: Colors.black87,
-                                fontWeight: FontWeight.w500,
-                                height: 1.6),
-                          ),
-                          subtitle: Text(
-                            'My phone number',
-                            style: TextStyle(
-                                fontSize: 15.0,
-                                fontFamily: "Ubuntu",
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w400,
-                                height: 1.5),
-                          ),
-                          contentPadding: EdgeInsets.all(0),
-                          dense: true,
-                        ),
-                      ),
-                    ),
-                    Card(
-                      elevation: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 5.0, horizontal: 20),
-                        child: ListTile(
-                          title: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'LOG OUT',
-                                style: TextStyle(
-                                    fontSize: 16.0,
-                                    fontFamily: "Ubuntu",
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.w400,
-                                    height: 1.6),
-                              ),
-                            ],
-                          ),
-                          contentPadding: EdgeInsets.all(0),
-                          dense: true,
-                          onTap: () {
-                            ConfirmAlertBoxDark(
-                                context: context,
-                                title: 'Are you sure you want to leave?',
-                                titleTextColor: Colors.white,
-                                icon: Icons.eighteen_mp,
-                                infoMessage: '',
-                                onPressedYes: () {
-                                  BlocProvider.of<AuthenticationBloc>(context)
-                                      .add(AuthenticationEvent.LOGGED_OUT);
-                                });
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+    return Stack(
+      fit: StackFit.passthrough,
+      children: <Widget>[
+        AnimatedContainer(
+          color: _bgColors[_pageIndex],
+          duration: const Duration(milliseconds: 200),
+          padding: const EdgeInsets.all(40.0),
+          child: PageView(
+            scrollDirection: Axis.vertical,
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() => _pageIndex = index);
+            },
+            children: _pinPuts.map((p) {
+              return FractionallySizedBox(
+                heightFactor: 1.0,
+                child: Center(child: p),
+              );
+            }).toList(),
           ),
         ),
-      );
-    });
+        _bottomAppBar,
+      ],
+    );
+  }
+
+  Widget onlySelectedBorderPinPut() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      color: const Color.fromRGBO(235, 236, 237, 1),
+      borderRadius: BorderRadius.circular(5.0),
+    );
+    return Form(
+      key: _formKey,
+      child: GestureDetector(
+        onLongPress: () {
+          print(_formKey.currentState.validate());
+        },
+        child: PinPut(
+          validator: (s) {
+            if (s.contains('1')) return null;
+            return 'NOT VALID';
+          },
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          withCursor: true,
+          fieldsCount: 5,
+          fieldsAlignment: MainAxisAlignment.spaceAround,
+          textStyle: const TextStyle(fontSize: 25.0, color: Colors.black),
+          eachFieldMargin: EdgeInsets.all(0),
+          eachFieldWidth: 45.0,
+          eachFieldHeight: 55.0,
+          onSubmit: (String pin) => _showSnackBar(pin),
+          focusNode: _pinPutFocusNode,
+          controller: _pinPutController,
+          submittedFieldDecoration: pinPutDecoration,
+          selectedFieldDecoration: pinPutDecoration.copyWith(
+            color: Colors.white,
+            border: Border.all(
+              width: 2,
+              color: const Color.fromRGBO(160, 215, 220, 1),
+            ),
+          ),
+          followingFieldDecoration: pinPutDecoration,
+          pinAnimationType: PinAnimationType.scale,
+        ),
+      ),
+    );
+  }
+
+  Widget darkRoundedPinPut() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      color: const Color.fromRGBO(25, 21, 99, 1),
+      borderRadius: BorderRadius.circular(20.0),
+    );
+    return PinPut(
+      eachFieldWidth: 65.0,
+      eachFieldHeight: 65.0,
+      withCursor: true,
+      fieldsCount: 4,
+      focusNode: _pinPutFocusNode,
+      controller: _pinPutController,
+      onSubmit: (String pin) => _showSnackBar(pin),
+      submittedFieldDecoration: pinPutDecoration,
+      selectedFieldDecoration: pinPutDecoration,
+      followingFieldDecoration: pinPutDecoration,
+      pinAnimationType: PinAnimationType.scale,
+      textStyle: const TextStyle(color: Colors.white, fontSize: 20.0),
+    );
+  }
+
+  Widget animatingBorders() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      border: Border.all(color: Colors.deepPurpleAccent),
+      borderRadius: BorderRadius.circular(15.0),
+    );
+    return PinPut(
+      fieldsCount: 5,
+      eachFieldHeight: 40.0,
+      withCursor: true,
+      onSubmit: (String pin) => _showSnackBar(pin),
+      focusNode: _pinPutFocusNode,
+      controller: _pinPutController,
+      submittedFieldDecoration: pinPutDecoration.copyWith(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      selectedFieldDecoration: pinPutDecoration,
+      followingFieldDecoration: pinPutDecoration.copyWith(
+        borderRadius: BorderRadius.circular(5.0),
+        border: Border.all(
+          color: Colors.deepPurpleAccent.withOpacity(.5),
+        ),
+      ),
+    );
+  }
+
+  Widget boxedPinPutWithPreFilledSymbol() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      color: const Color.fromRGBO(119, 125, 226, 1),
+      borderRadius: BorderRadius.circular(5.0),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(3.0),
+        border: Border.all(color: Colors.white),
+      ),
+      padding: const EdgeInsets.all(20.0),
+      child: PinPut(
+        withCursor: true,
+        fieldsCount: 5,
+        preFilledWidget: FlutterLogo(),
+        textStyle: const TextStyle(fontSize: 25.0, color: Colors.white),
+        eachFieldWidth: 50.0,
+        eachFieldHeight: 50.0,
+        onSubmit: (String pin) => _showSnackBar(pin),
+        focusNode: _pinPutFocusNode,
+        controller: _pinPutController,
+        submittedFieldDecoration: pinPutDecoration,
+        selectedFieldDecoration: pinPutDecoration.copyWith(color: Colors.white),
+        followingFieldDecoration: pinPutDecoration,
+      ),
+    );
+  }
+
+  Widget justRoundedCornersPinPut() {
+    final BoxDecoration pinPutDecoration = BoxDecoration(
+      color: const Color.fromRGBO(43, 46, 66, 1),
+      borderRadius: BorderRadius.circular(10.0),
+      border: Border.all(
+        color: const Color.fromRGBO(126, 203, 224, 1),
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: PinPut(
+        fieldsCount: 4,
+        withCursor: true,
+        textStyle: const TextStyle(fontSize: 25.0, color: Colors.white),
+        eachFieldWidth: 40.0,
+        eachFieldHeight: 55.0,
+        onSubmit: (String pin) => _showSnackBar(pin),
+        focusNode: _pinPutFocusNode,
+        controller: _pinPutController,
+        submittedFieldDecoration: pinPutDecoration,
+        selectedFieldDecoration: pinPutDecoration,
+        followingFieldDecoration: pinPutDecoration,
+        pinAnimationType: PinAnimationType.fade,
+      ),
+    );
+  }
+
+  Widget get _bottomAppBar {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          FlatButton(
+            onPressed: () => _pinPutFocusNode.requestFocus(),
+            child: const Text('Focus'),
+          ),
+          FlatButton(
+            onPressed: () => _pinPutFocusNode.unfocus(),
+            child: const Text('Unfocus'),
+          ),
+          FlatButton(
+            onPressed: () => _pinPutController.text = '',
+            child: const Text('Clear All'),
+          ),
+          FlatButton(
+            child: Text('Paste'),
+            onPressed: () => _pinPutController.text = '234',
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showSnackBar(String pin) {
+    final snackBar = SnackBar(
+      duration: const Duration(seconds: 3),
+      content: Container(
+        height: 80.0,
+        child: Center(
+          child: Text(
+            'Pin Submitted. Value: $pin',
+            style: const TextStyle(fontSize: 25.0),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.deepPurpleAccent,
+    );
+    Scaffold.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(snackBar);
   }
 }
